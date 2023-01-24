a7f747a8688aacbdfa8e93e8dd68e2cf517c8b17
lana <diegogarciaq98@gmail.com>
2023-01-23 19:35:15 -0500

    Add contact page and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get '/portfolio' => 'portfolio#portfolio'
   get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
